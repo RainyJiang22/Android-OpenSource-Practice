0de4c971a2e1bdcf670e1ac0bf56e23af24a3649
jiangshiyu <3434481891@qq.com>
2022-11-30 18:45:50 +0800

    ADD:添加通知样式Notification

--- FlowExample/src/main/java/com/rainy/flow/example/flowtest/FlowTest.kt
+++ FlowExample/src/main/java/com/rainy/flow/example/flowtest/FlowTest.kt
@@ -1,15 +1,29 @@
 package com.rainy.android_opensource_practice.flowtest
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
 /**
  * @author jiangshiyu
@@ -27,6 +41,8 @@ object FlowTest {
     }.onErrorReturn {
         throw Exception(it.message)
     }
+        .subscribeOn(Schedulers.io()) //指定被观察者（发送的事件）在io线程
+        .observeOn(AndroidSchedulers.mainThread()) //指定观察者（接收订阅的事件）在主线程
 
 
     /**
@@ -42,6 +58,8 @@ object FlowTest {
         throw Exception(it.message)
     }
 
+
+    //创建flow的不同种方式
 
     fun createFlow() {
         CoroutineScope(Dispatchers.Main.immediate).launch {
@@ -62,6 +80,51 @@ object FlowTest {
                     println("the thread is ${Thread.currentThread()}")
                     emit("this is three")
                 }
+
+
+            listOf(1, 2, 3, 4).asFlow().onEach {
+                delay(1000L)
+            }.collect {
+                println(it)
+            }
+
+            @OptIn(ExperimentalCoroutinesApi::class)
+            channelFlow {
+                for (i in 1..5) {
+                    delay(1000L)
+                    send(i)
+                }
+            }.collect {
+                println(it)
+            }
         }
+    }
+
+
+}
+
+fun currentTime() = System.currentTimeMillis()
+fun threadName() = Thread.currentThread().name
+var start: Long = 0
+
+fun createEmitter(): Flow<Int> =
+    (1..5)
+        .asFlow()
+        .onStart { start = currentTime() }
+        .onEach {
+            delay(1000L)
+            print("Emit $it (${currentTime() - start}ms) ")
+        }
+
+fun main() {
+    runBlocking {
+        val time = measureTimeMillis {
+            createEmitter().collectLatest {
+                print("\nCollect $it starts ${start - currentTime()}ms")
+                delay(3000L)
+                println("   Collect $it ends ${currentTime() - start}ms")
+            }
+        }
+        print("\nCollected in $time ms")
     }
 }
---
+++ app/src/main/java/com/rainy/android_opensource_practice/picture/TestActivity.kt
@@ -0,0 +1,139 @@
+package com.rainy.android_opensource_practice.picture
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.luck.picture.lib.basic.PictureSelector
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.config.SelectModeConfig
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
+import com.rainy.android_opensource_practice.BaseActivity
+import com.rainy.android_opensource_practice.MainActivity
+import com.rainy.android_opensource_practice.R
+import com.rainy.android_opensource_practice.databinding.ActivityTestBinding
+import me.hgj.jetpackmvvm.base.viewmodel.BaseViewModel
+import kotlin.collections.ArrayList
+
+/**
+ * @author jiangshiyu
+ * @date 2022/11/16
+ */
+class TestActivity : BaseActivity<BaseViewModel, ActivityTestBinding>() {
+
+    companion object {
+        const val TAG = "TestActivity"
+
+
+        private const val mHighNotificationId = 9002
+        private const val mStopAction = "com.widget.stop" // 暂停继续action
+        private const val mDoneAction = "com.widget.done" // 完成action
+        private var mFlag = 0
+        private var mIsStop = false // 是否在播放 默认未开始
+
+    }
+
+
+    private val mHighChannelId = "重要渠道id"
+    private val mHighChannelName = "重要通知"
+    private var mReceiver: NotificationReceiver? = null
+
+    private lateinit var mManager: NotificationManager
+    private lateinit var mBuilder: NotificationCompat.Builder
+
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+        mManager = this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        createReceiver()
+
+
+        mViewBind.btnAlbum.setOnClickListener {
+
+            PictureSelector.create(this)
+                .openSystemGallery(SelectMimeType.ofAll())
+                .setSelectionMode(SelectModeConfig.SINGLE)
+                .forSystemResult(object : OnResultCallbackListener<LocalMedia> {
+                    override fun onResult(result: ArrayList<LocalMedia>?) {
+                        Log.d(TAG, "onResult: ${result.toString()}")
+                    }
+
+                    override fun onCancel() {
+                        Log.d(TAG, "onCancel: ")
+                    }
+
+                })
+        }
+
+        mViewBind.btnNotification.setOnClickListener {
+            createNotificationForMessage()
+        }
+    }
+
+
+    /**
+     * 发起重要通知
+     */
+    private fun createNotificationForMessage() {
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_IMMUTABLE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(mHighChannelId,
+                mHighChannelName,
+                NotificationManager.IMPORTANCE_HIGH)
+            channel.setShowBadge(true)
+            mManager.createNotificationChannel(channel)
+        }
+        mBuilder = NotificationCompat.Builder(this, mHighChannelId)
+            .setContentTitle("消息通知")
+            .setContentText("你提交的反馈有了新的回复")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setAutoCancel(true)
+            .addAction(R.mipmap.ic_launcher, "去看看", pendingIntent)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE) // 通知类别，"勿扰模式"时系统会决定要不要显示你的通知
+            .setVisibility(NotificationCompat.VISIBILITY_PRIVATE) // 屏幕可见性，锁屏时，显示icon和标题，内容隐藏
+        mManager.notify(mHighNotificationId, mBuilder.build())
+    }
+
+
+    /**
+     * 创建广播接收器
+     */
+    private fun createReceiver() {
+        val intentFilter = IntentFilter()
+        // 添加接收事件监听
+        intentFilter.addAction(mStopAction)
+        intentFilter.addAction(mDoneAction)
+        mReceiver = NotificationReceiver()
+        // 注册广播
+        registerReceiver(mReceiver, intentFilter)
+    }
+
+    private class NotificationReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            // 拦截接收事件
+            if (intent.action == mStopAction) {
+                // 改变状态
+                mIsStop = !mIsStop
+
+            } else if (intent.action == mDoneAction) {
+                Toast.makeText(context, "完成", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
