182c05b2254c027c5df5bf84d1a3eebbca8cf35b
jiangshiyu <3434481891@qq.com>
2022-12-12 18:04:00 +0800

    ADD:Flow Backpressure

--- FlowExample/src/main/java/com/rainy/flow/example/flowtest/FlowTest.kt
+++ FlowExample/src/main/java/com/rainy/flow/example/flowtest/FlowTest.kt
@@ -1,12 +1,18 @@
 package com.rainy.android_opensource_practice.flowtest
 
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.FlowableEmitter
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
@@ -16,9 +22,11 @@ import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
@@ -29,7 +37,7 @@ import kotlin.system.measureTimeMillis
  * @author jiangshiyu
  * @date 2022/11/3
  */
-object FlowTest {
+class FlowTest {
 
 
     fun updateObservable(): Observable<String> = Observable.create<String?> {
@@ -45,10 +53,38 @@ object FlowTest {
         .observeOn(AndroidSchedulers.mainThread()) //指定观察者（接收订阅的事件）在主线程
 
 
+    //completable in Rxjava
+    fun completableRequest(): Completable {
+        return Completable.create { emitter ->
+            try {
+                emitter.onComplete()
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }
+    }
+
+    fun main() {
+        completableRequest()
+            .subscribe {
+                println("I,am done")
+                println()
+            }
+    }
+
+    fun completableCoroutine() = runBlocking {
+        try {
+            delay(500L)
+            println("I am done")
+        } catch (e: Exception) {
+            println("Got an exception")
+        }
+    }
+
+
     /**
      * 替换成flow
      */
-
     fun updateFlow() = flow {
         emit("step 1")
         emit("step 2")
@@ -126,5 +162,168 @@ fun main() {
             }
         }
         print("\nCollected in $time ms")
+    }
+}
+
+/*fun getUsernames(): Flowable<String> {
+    return Flowable<String>()
+}
+
+
+fun getUserFromNetwork(): Single<User> {
+    //.....
+}*/
+
+
+fun userNameFlow() = flow<User> {
+    return@flow
+}
+
+
+data class User(val userName: String)
+
+
+fun compareFlatMap() {
+    /*getUsernames() //Flowable<String>
+        .flatMapSingle { username ->
+            getUserFromNetwork(username) // Single<User>
+        }
+        .subscribe(
+            { user -> println(user) },
+            { println("Got an exception") }
+        )*/
+    runBlocking {
+        flow {
+            emit(User("Jacky"))
+        }.map {
+            getUserFromName(it)
+        }.collect {
+            println(it)
+        }
+    }
+
+
+}
+
+suspend fun getUserFromName(user: User): String {
+    return user.userName
+}
+
+fun getUsernames(): Flowable<String> {
+    val flowableEmitter = { emitter: FlowableEmitter<String> ->
+        emitter.onNext("Jacky")
+    }
+    return Flowable.create(flowableEmitter, BackpressureStrategy.BUFFER)
+}
+
+fun isCorrectUserName(userName: String): Single<Boolean> {
+    return Single.create { emitter ->
+        runCatching {
+            //名字判断....
+            if (userName.isNotEmpty()) {
+                emitter.onSuccess(true)
+            } else {
+                emitter.onSuccess(false)
+            }
+        }.onFailure {
+            emitter.onError(it)
+        }
+    }
+}
+
+
+fun compareFilter() {
+    getUsernames()//Flowable<String>
+        .flatMapSingle { userName ->
+            isCorrectUserName(userName) //Single<Boolean>
+                .flatMap { isCorrect ->
+                    if (isCorrect) {
+                        Single.just(userName)
+                    } else {
+                        Single.never()
+                    }
+                }
+        }.subscribe {
+            println(it)
+        }
+
+    runBlocking {
+        userNameFlow().filter { user ->
+            isCorrectName(user.userName)
+        }.collect { user ->
+            println(user)
+        }
+    }
+
+}
+
+suspend fun isCorrectName(userName: String): Boolean {
+    return userName.isNotEmpty()
+}
+
+fun RxBackpressureDropTest() {
+    Flowable.range(1, 1_000_000)
+        .onBackpressureDrop()
+        .observeOn(Schedulers.single(), false, 1)
+        .subscribe { value ->
+            Thread.sleep(100)
+            println("Got value:$value")
+        }
+
+    Thread.sleep(1000)
+
+}
+
+fun FlowBackpressureDropTest() {
+    runBlocking {
+        (1..1_000_000).asFlow()
+            .buffer(capacity = 1, onBufferOverflow = BufferOverflow.DROP_LATEST)
+            .collect { value ->
+                delay(100)
+                println("Got value: $value")
+            }
+    }
+}
+
+fun RxBackpressureLatestTest() {
+    Flowable.range(1, 1_000_000)
+        .onBackpressureLatest()
+        .observeOn(Schedulers.single(), false, 2)
+        .subscribe { value ->
+            Thread.sleep(100)
+            println("Got value: $value")
+        }
+    Thread.sleep(1000L)
+}
+
+fun FlowBackpressureLatestTest() {
+    runBlocking {
+        (1..1_000_000).asFlow()
+            .buffer(capacity = 2, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+            .collect { value ->
+                delay(100)
+                println("Got value: $value")
+            }
+    }
+}
+
+fun RxBackpressureBufferTest() {
+    Flowable.range(1, 1_000_000)
+        .onBackpressureBuffer()
+        .observeOn(Schedulers.single())
+        .subscribe { value ->
+            Thread.sleep(100)
+            println("Got value: $value")
+        }
+}
+
+fun FlowBackpressureBufferTest() {
+    runBlocking {
+        (1..1_000_00).asFlow()
+            .buffer(capacity = 0, onBufferOverflow = BufferOverflow.SUSPEND)
+            .collect { value ->
+                delay(100)
+                println("Got value: $value")
+            }
     }
 }
